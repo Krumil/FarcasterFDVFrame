f38a1c0822240a2a0576bc2802a6a4b958802199
s.saletti <s.saletti@qzrstudio.com>
2024-05-22 15:24:52 +0200

    push

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -4,11 +4,7 @@ import { frames } from "./frames";
 
 const handleRequest = frames(async ctx => {
 	return {
-		image: (
-			<div tw='text-white w-full h-full justify-center items-center flex bg-slate-900'>
-				Compare every coin market cap
-			</div>
-		),
+		image: <div tw='text-white w-full h-full justify-center items-center flex bg-slate-900'>Compare every FDV</div>,
 		buttons: [
 			<Button action='post' target={{ pathname: "/route1" }}>
 				Next Step
